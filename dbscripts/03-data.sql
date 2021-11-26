02c78d28e6785f9e2e68920319d72979c4cd9943
palsokcoc <60387154+palsokcoc@users.noreply.github.com>
2021-11-27 01:29:44 +0300

    db dosyaları eklendi

---
+++ dbscripts/03-data.sql
@@ -0,0 +1,34 @@
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-1, 'Ankara', 'Akyurt', 'Uzunlar', 'Atatürk', 52, 4);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-2, 'Antalya', 'Manavgat', 'Barbaros', 'Cumhuriyet', 104, 13);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-3, 'Ankara', 'Altındağ', 'Çağlayan', 'Fatih', 74, 1);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-4, 'Balıkesir', 'Dursunbey', 'Hasanlar', 'Gül', 8, 26);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-5, 'Diyarbakır', 'Bismil', 'Yasince', 'Okul', 85, 14);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-6, 'Erzurum', 'Aziziye', 'Eğerti', 'Karanfil', 47, 10);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-7, 'İzmir', 'Kemalpaşa', 'Dereköy', 'Lale', 52, 3);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-8, 'Kırşehir', 'Mucur', 'Hamidiye', 'Menekşe', 21, 25);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-9, 'Manisa', 'Soma', 'Hürriyet', 'İnönü', 14, 27);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-10, 'Ankara', 'Çankaya', 'Oğuzlar', 'Anafartalar', 3, 30);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-11, 'Ankara', 'Keçiören', 'Çiçekli', 'İstiklal', 44, 33);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-12, 'İstanbul', 'Bağcılar', 'Demirkapı', 'Sokullu', 79, 5);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-13, 'İstanbul', 'Çatalca', 'Başak', 'Bahariye', 96, 2);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-14, 'Malatya', 'Battalgazi', 'Pelitli', 'Girne', 124, 11);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-15, 'Sakarya', 'Pamukova', 'Çakmaklı', 'Kıbrıs', 35, 18);
+insert into iskolik.adres (adres_id,sehir,ilce,mahalle,cadde,apartman_no,daire_no) values(-16, 'Samsun', 'İlkadım', 'Kılıçdede', 'Bahçeli', 68, 23);
+
+
+
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Bilişim Teknolojileri', 'Bilişim Teknolojileri', null, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Yazılım Geliştirme', 'Yazılım Geliştirme', 1, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Programlama Dili', 'Programlama Dili', 2, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('C', 'C', 3, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('C++', 'C++', 3, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Java', 'Java', 3, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Spring', 'Spring', 6, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Hibernate', 'Hibernate', 6, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('JUnit', 'JUnit', 6, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('JavaScript', 'JavaScript', 3, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Node.js', 'Node.js', 10, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Vue.js', 'Vue.js', 10, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('React.js', 'React.js', 10, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Python', 'Python', 3, 'Aktif');
+insert into iskolik.kategori (ad,aciklama,ata_kategori_id,durum) values('Diğer', 'Diğer', 3, 'Aktif');
